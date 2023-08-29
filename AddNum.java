266088842b544a1ed17a886b6be9ba25cc33f311
V-B-SAKTHIVEL <112497833+V-B-SAKTHIVEL@users.noreply.github.com>
2023-08-29 22:35:08 +0530

    Add files via upload
    Program to add the given two numbers in Java.

---
+++ AddNum.java
@@ -0,0 +1,9 @@
+class AddNum
+{
+    public static void main(String args[])
+    {
+        double a = 5, b = 6, c;
+        c = a + b;
+        System.out.println("The Addition of two numbers is : " + c);
+    }
+}
