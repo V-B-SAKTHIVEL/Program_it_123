f44f5e2fd81b63fda57d1b864e6146293e5da3bc
V-B-SAKTHIVEL <112497833+V-B-SAKTHIVEL@users.noreply.github.com>
2023-08-30 18:09:47 +0530

    Add files via upload

---
+++ MulNum.java
@@ -0,0 +1,11 @@
+public class MulNum
+{
+	public static void main(String[] args) 
+	{
+		int a = 5, b = 7, c;
+		System.out.println("Program to multiply the given two numbers");
+		System.out.println("\na = " + a + " b = " + b);
+		c = a * b;
+		System.out.println("Answer is : " + c);
+	}
+}
---
+++ SubNum.java
@@ -0,0 +1,11 @@
+class SubNum
+{
+    public static void main(String args[])
+    {
+        int a = 5, b = 2, c;
+        c = a - b;
+        System.out.println("Program to subtract the given numbers");
+        System.out.println("\na = " + a + " b = " + b);
+        System.out.println("Answer is " + c);
+    }
+}
