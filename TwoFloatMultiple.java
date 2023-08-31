34bd531df623c4f4176351b57fc4501f16f5f927
V-B-SAKTHIVEL <112497833+V-B-SAKTHIVEL@users.noreply.github.com>
2023-08-31 19:16:56 +0530

    Add files via upload
    Program to add two float data type values in Java.

---
+++ TwoFloatMultiple.java
@@ -0,0 +1,9 @@
+class TwoFloatMultiple
+{
+    public static void main(String args[])
+    {
+        System.out.println("Program to display the multiplication of float type of data");
+        float f1 = 12.35f, f2 = 35.76f;
+        System.out.println("\nf1 = " + f1 + " f2 = " + f2 + "\nAddition = " + (f2+f1));
+    }
+}
