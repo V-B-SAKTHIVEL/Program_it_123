e599492c056093f055f6fe36763754f849c77b8f
V-B-SAKTHIVEL <112497833+V-B-SAKTHIVEL@users.noreply.github.com>
2023-09-03 18:02:58 +0530

    Add files via upload
    Program to check the Area of Rectangle in Java.

---
+++ AreaRectangle.java
@@ -0,0 +1,11 @@
+class AreaRectangle
+{
+   public static void main(String args[]) 
+   {
+      double length = 2.55, breadth = 5.53, area;
+      System.out.println("Program to calculate the Area of Rectangle. ") ;
+
+      area = length * breadth;
+      System.out.println("length = " + length + "breadth =  " + breadth + "\nArea is : " + area);
+   }
+}
