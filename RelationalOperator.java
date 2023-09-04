0085eb5a7403a24eaa2e6d3fbd32e7f48d757944
V-B-SAKTHIVEL <112497833+V-B-SAKTHIVEL@users.noreply.github.com>
2023-09-04 19:24:06 +0530

    Add files via upload
    Program to illustrate the concept of Relational Operator in Java.

---
+++ RelationalOperator.java
@@ -0,0 +1,14 @@
+class RelationalOperator
+{
+    public static void main(String args[])
+    {
+        int a = 5, b = 7;
+        System.out.println("a = " + a + " b = " + b);
+        System.out.println("\na =  b is " + (a==b));
+        System.out.println("a != b is " + (a != b));
+        System.out.println("a > b is " + (a > b));
+        System.out.println("a < b is " + (a < b));
+        System.out.println("a >= b is " + (a >= b));
+        System.out.println("a <= b is " + (a <= b));
+    }
+}
