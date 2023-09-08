a2581aa38e9cd0d9f47b4825f506886b6bc5a75a
V-B-SAKTHIVEL <112497833+V-B-SAKTHIVEL@users.noreply.github.com>
2023-09-08 18:56:25 +0530

    Add files via upload

---
+++ ElectionCheck.java
@@ -0,0 +1,13 @@
+class ElectionCheck
+{
+   public static void main(String args[])
+   {
+      System.out.println("Program to check whether the person is eligible to vote or not using if statement.");
+ 
+      int age = 16;
+      if(age >= 18)
+      {
+         System.out.println("You are eligible for voting...");
+      }
+   }
+}
---
+++ ElectionCheck2.java
@@ -0,0 +1,18 @@
+class ElectionCheck2
+{
+   public static void main(String args[])
+   {
+      System.out.println("Program to check whether the person is eligible to vote or not using if...else statement.");
+
+      int age = 16;
+      if(age >= 18)
+      {
+         System.out.println("You are eligible for voting...");
+      }
+
+      else
+      {
+         System.out.println("You are not eligible for voting...");
+      }
+   }
+}
